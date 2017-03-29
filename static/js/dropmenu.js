1a301770d50b54002385e645bd685cc34b33d636
Andres Lopez <andres15-14@utp.edu.co>
2017-03-29 14:23:50 -0500

    SideMenu "FixBug"

--- static/js/dropmenu.js
+++ static/js/dropmenu.js
@@ -1,8 +1,8 @@
- // Initialize collapse button
-$(".button-collapse").sideNav();
+$(function sideMenu(){
+   // Initialize collapse button
+  $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
-
   $('.button-collapse').sideNav({
       menuWidth: 200, // Default is 300
       edge: 'left', // Choose the horizontal origin
@@ -10,10 +10,10 @@ $(".button-collapse").sideNav();
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
-
    // Show sideNav
  $('.button-collapse').sideNav('show');
   // Hide sideNav
   $('.button-collapse').sideNav('hide');
   // Destroy sideNav
   $('.button-collapse').sideNav('destroy');
+})
