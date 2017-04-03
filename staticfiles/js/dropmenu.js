8dfa0b929c798b9d3f4fc39a4c9792f6477e1501
Andres Lopez <andres15-14@utp.edu.co>
2017-04-02 21:28:36 -0500

    cambios no reflejados en remoto

--- staticfiles/js/dropmenu.js
+++ staticfiles/js/dropmenu.js
@@ -6,12 +6,12 @@ $(function sideMenu(){
   $('.button-collapse').sideNav({
       menuWidth: 200, // Default is 300
       edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens
     }
   );
    // Show sideNav
- $('.button-collapse').sideNav('show');
+ $.click('.button-collapse').sideNav('show');
   // Hide sideNav
   $('.button-collapse').sideNav('hide');
   // Destroy sideNav
